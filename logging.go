4962ed3250ea3449d72d8adee21516dd6328f736
Andrew Wyatt <arcturus615@gmail.com>
2020-06-22 06:39:39 -0400

    Change comments and function order

--- logging.go
+++ logging.go
@@ -34,6 +34,7 @@ type Loggable interface {
 	UUID() string
 }
 
+// sendToChans sends the given string to the provided list of channels
 func sendToChans(out string, chs []chan []byte) {
 	for _, ch := range chs {
 		select {
@@ -41,77 +42,6 @@ func sendToChans(out string, chs []chan []byte) {
 		default:
 			log.Output(1, "Unable to send to closed channel!")
 		}
-	}
-}
-
-// LogOutput - Log the given string with a timestamp and no prefix. Logging does
-// not depend on the current loglevel of an object
-func LogOutput(l Loggable, m string, chs ...chan []byte) {
-	log.Output(1, m)
-}
-
-// LogError - Log an error.
-func LogError(l Loggable, m string, chs ...chan []byte) {
-	m = sprintf("[%s] %s", errorPrefix, m)
-	log.Output(1, m)
-	sendToChans(m, chs)
-}
-
-// LogWarning - Log a warning
-func LogWarning(l Loggable, m string, chs ...chan []byte) {
-	m = sprintf("[%s] %s", warnPrefix, m)
-	log.Output(1, m)
-	sendToChans(m, chs)
-}
-
-// LogDebug - Log a debug message if the loglevel of the given object is three
-// or greater
-func LogDebug(l Loggable, m string, chs ...chan []byte) {
-	if l.Loglevel() >= debugLevel {
-		log.Output(1, sprintf("[%s] %s", debugPrefix, m))
-	}
-}
-
-// LogInfo - Log an informational notification if the loglevel is one or greater
-func LogInfo(l Loggable, m string, chs ...chan []byte) {
-	if l.Loglevel() >= infoLevel {
-		log.Output(1, sprintf("[%s] %s", infoPrefix, m))
-	}
-	sendToChans(sprintf("[%s] %s", infoPrefix, m), chs)
-}
-
-// LogInit - Log an initialization message
-func LogInit(l Loggable, m string, chs ...chan []byte) {
-	m = sprintf("[%s] %s", initPrefix, m)
-	log.Output(1, m)
-	sendToChans(m, chs)
-}
-
-// LogVerbose - Log a message only when the loglevel of an object is 2 or greater
-func LogVerbose(l Loggable, m string, chs ...chan []byte) {
-	if l.Loglevel() >= verboseLevel {
-		log.Output(1, sprintf("[%s] %s", verbosePrefix, m))
-	}
-}
-
-// LogChat - Log server chat
-func LogChat(l Loggable, m string, chs ...chan []byte) {
-	if l.Loglevel() >= infoLevel {
-		log.Output(1, sprintf("[%s] %s", chatPrefix, m))
-	}
-	sendToChans(sprintf("[%s] %s", chatPrefix, m), chs)
-}
-
-// LogHTTP - Log an HTTP response code and string. Provides formatting for the
-// response, and will output if the loglevel of the object is 1 or greater
-func LogHTTP(l Loggable, rc int, r *http.Request, chs ...chan []byte) {
-	if l.Loglevel() >= infoLevel {
-		rcs := formatResponseHeader(rc, r.Method)
-		rinfo := sprintf("%s - %s %s",
-			r.RemoteAddr,
-			r.Host,
-			r.RequestURI)
-		log.Output(1, sprintf("%s %s", rcs, rinfo))
 	}
 }
 
@@ -134,5 +64,76 @@ func formatResponseHeader(r int, m string) string {
 		return yellowbg(black(out))
 	default:
 		return white(redbg(out))
+	}
+}
+
+// LogOutput logs the given string with a timestamp and no prefix. Logging does
+// not depend on the current loglevel of an object
+func LogOutput(l Loggable, m string, chs ...chan []byte) {
+	log.Output(1, m)
+}
+
+// LogError logs an error.
+func LogError(l Loggable, m string, chs ...chan []byte) {
+	m = sprintf("[%s] %s", errorPrefix, m)
+	log.Output(1, m)
+	sendToChans(m, chs)
+}
+
+// LogWarning logs a warning
+func LogWarning(l Loggable, m string, chs ...chan []byte) {
+	m = sprintf("[%s] %s", warnPrefix, m)
+	log.Output(1, m)
+	sendToChans(m, chs)
+}
+
+// LogDebug logs a debug message if the loglevel of the given object is three
+// or greater
+func LogDebug(l Loggable, m string, chs ...chan []byte) {
+	if l.Loglevel() >= debugLevel {
+		log.Output(1, sprintf("[%s] %s", debugPrefix, m))
+	}
+}
+
+// LogInfo logs an informational notification if the loglevel is one or greater
+func LogInfo(l Loggable, m string, chs ...chan []byte) {
+	if l.Loglevel() >= infoLevel {
+		log.Output(1, sprintf("[%s] %s", infoPrefix, m))
+	}
+	sendToChans(sprintf("[%s] %s", infoPrefix, m), chs)
+}
+
+// LogInit logs an initialization message
+func LogInit(l Loggable, m string, chs ...chan []byte) {
+	m = sprintf("[%s] %s", initPrefix, m)
+	log.Output(1, m)
+	sendToChans(m, chs)
+}
+
+// LogVerbose logs a message only when the loglevel of an object is 2 or greater
+func LogVerbose(l Loggable, m string, chs ...chan []byte) {
+	if l.Loglevel() >= verboseLevel {
+		log.Output(1, sprintf("[%s] %s", verbosePrefix, m))
+	}
+}
+
+// LogChat logs server chat
+func LogChat(l Loggable, m string, chs ...chan []byte) {
+	if l.Loglevel() >= infoLevel {
+		log.Output(1, sprintf("[%s] %s", chatPrefix, m))
+	}
+	sendToChans(sprintf("[%s] %s", chatPrefix, m), chs)
+}
+
+// LogHTTP logs an HTTP response code and string. Provides formatting for the
+// response, and will output if the loglevel of the object is 1 or greater
+func LogHTTP(l Loggable, rc int, r *http.Request, chs ...chan []byte) {
+	if l.Loglevel() >= infoLevel {
+		rcs := formatResponseHeader(rc, r.Method)
+		rinfo := sprintf("%s - %s %s",
+			r.RemoteAddr,
+			r.Host,
+			r.RequestURI)
+		log.Output(1, sprintf("%s %s", rcs, rinfo))
 	}
 }
